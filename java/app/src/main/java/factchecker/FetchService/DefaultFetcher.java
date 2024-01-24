0ee6fce571c46ebb1a985dca73526eb0c014067b
Sergey Kudashev <kudashevs@gmail.com>
2024-01-24 20:24:12 +0100

    Update DefaultFetcher add the default implementation

--- java/app/src/main/java/factchecker/FetchService/DefaultFetcher.java
+++ java/app/src/main/java/factchecker/FetchService/DefaultFetcher.java
@@ -1,8 +1,24 @@
 package factchecker.FetchService;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class DefaultFetcher implements Fetcher {
     @Override
-    public String fetch(String url) {
-        return "";
+    public String fetch(String url) throws IOException, InterruptedException {
+        var uri = URI.create(url);
+        HttpRequest request = HttpRequest.newBuilder(uri)
+            .header("accept", "application/json")
+            .timeout(Duration.ofSeconds(3))
+            .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient()
+            .send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
     }
 }
